4ee9f3ab58b4f684cd9cffd0e496fff0da75af3f
Falren <nokuchie@gmail.com>
2020-08-07 14:53:59 +0300

    Add users, products && orders controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,50 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to orders_path(@order)
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.upadte(order_params)
+      redirect_to order_path(@order)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @order = order.find(params[:id])
+    if @order.destroy
+      redirect_to orders_path
+    else
+      render 'show'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:user_id, :note)
+  end
 end
