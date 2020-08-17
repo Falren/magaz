017b9e15dbc66ce606a3ae503859ef708c1a8813
Falren <nokuchie@gmail.com>
2020-08-17 18:31:55 +0300

    fix indentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :orders
   has_one :address, as: :addressable
   has_one :drafted_order, -> { drafted }, class_name: 'Order'
+
   accepts_nested_attributes_for :address
 end
