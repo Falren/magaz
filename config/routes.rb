c219b1cd77ca822c2d691405cf1437e2cfffde01
Falren <nokuchie@gmail.com>
2020-08-18 16:06:19 +0300

    Fix address fields

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :orders
   resources :products
-  resources :users, only: %i[edit show update]
+  resources :users, except: :delete
   resources :categories
   resources :line_items
   root 'home#index'
