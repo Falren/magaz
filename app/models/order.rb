6f93ac427852aa5ba3f05de1e2d286cb700a66f4
Falren <nokuchie@gmail.com>
2020-08-24 16:52:23 +0300

    Add wish_list forms && minor changes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  enum status: %i[drafted completed]
+  enum status: %i[drafted completed archived]
 
   belongs_to :user
   has_many :line_items
@@ -7,7 +7,6 @@ class Order < ApplicationRecord
 
   has_one :address, as: :addressable
   accepts_nested_attributes_for :address
-
   delegate :post_index, :number, :building_address, to: :address, prefix: false, allow_nil: true
   delegate :id, to: :address, prefix: true, allow_nil: true
 end
