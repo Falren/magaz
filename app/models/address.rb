4a13a95b5a7a35649351e47be12243632037be0e
Falren <nokuchie@gmail.com>
2020-09-02 17:50:56 +0300

    Minor fixes

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
-  validates_presence_of :building_address, :post_index, :number, presence: true
+  validates_presence_of :building_address, :post_index, :number
 end
