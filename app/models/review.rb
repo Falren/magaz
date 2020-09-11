1045905b0d6fa438222a7626027ca7286273e902
Falren <nokuchie@gmail.com>
2020-09-11 17:37:17 +0300

    fix edited

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,8 @@ class Review < ApplicationRecord
   scope :created_since, ->(time) { where('created_at > ?', time) if time.present? }
   scope :most_recent, ->(limit) { order(created_at: :desc).limit(limit) }
   scope :rated_product, ->(rating) { where('rating > ?', rating) if rating.present? }
+
+  def edited?
+    updated_at != created_at
+  end
 end
